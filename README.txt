16680c6e222442d3ef9c5824065a9abd7e7d0841
anoopajay91 <anoopajay91@gmail.com>
2014-05-09 19:39:46 +0530

    Dowload lyrics

---
+++ README.txt
@@ -0,0 +1,20 @@
+Dependancies:
+
+1.Python
+2.BeautifulSoup module
+3.Requests module
+
+The file will find the best match and gives user an option to place the
+lyrics into a text file with file name of their choice.
+
+User can find the best match by giving proper song name along with the aritst name
+Ex: careless whisper george michael is a good input. It mentions both song and the singer
+
+Change the dictionary value of keywords['drive'] to the location where you
+would want to save. Ex: C:// or D://
+
+Have fun :)
+
+
+
+
---
+++ lyricsDownloader.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+keywords={"http":'http',
+          "end":"html",
+          "style":'margin-left:10px;margin-right:10px;',
+          "Lyricsend":"</div",
+          "drive":"L://lyrics//",
+          "input":'y'
+        }
+while(keywords['input'].lower()=='y'):
+    song = raw_input("enter song's name\n")
+
+    r = requests.get("http://www.google.com/search?q="+song.replace(' ','+')+'+lyrics')
+    asciiText = r.text.encode('ascii','ignore') # converting to ascii
+    r.close()
+    soup = BeautifulSoup(asciiText)
+    slink=''
+    elms = soup.select("h3 a")
+    
+    for each in elms:
+        if str(each.attrs["href"]).find('azlyrics')>0:
+            slink = str(each.attrs["href"])
+            slink = slink[slink.find(keywords['http']):slink.find(keywords["end"])+len(keywords['end'])]
+
+    if slink:
+        r = requests.get(slink)
+        page = r.text.encode('ascii','ignore') # converting to ascii
+        r.close()
+        soup = BeautifulSoup(page)
+        whole = soup.findAll("div",{'style':keywords['style']})
+        fileName = raw_input("enter the file name to which you wanna save: ")
+        
+        with open(keywords['drive']+fileName+'.txt','w+') as f:
+            f.write(whole[0].text)
+        print "your file has been saved to "+keywords['drive']+fileName+'.txt'
+        print
+        #print whole[0].text
+    else:
+        print "Sorry, couldn't locate on server"
+
+    keywords['input'] = raw_input('do you wanna check any other song y/n: ').lower()
